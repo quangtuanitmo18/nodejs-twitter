1b2a1cf88dd8af3a44df279931c6e907b1a5ac4b
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-07-13 14:41:20 +0300

    tổ chức thư mục

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-const name1: string = 'Dư Thanh Được'
-console.log(name1)
 import express from 'express'
-import userRouter from '~/user.routes'
+import usersRouter from './routes/users.routes'
 const app = express()
 const port = 3000
 
-app.post('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.use('/user', userRouter)
-
+app.use(express.json())
+app.use('/users', usersRouter)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
