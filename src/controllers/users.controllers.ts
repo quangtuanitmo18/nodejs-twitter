f71edb07c05d21fc498f2d6ad8914e1f915ef9a1
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-07-14 20:00:53 +0300

    logic login

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -2,16 +2,17 @@ import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/User.requests'
 import { NextFunction, Request, Response } from 'express'
+import { USERS_MESSAGES } from '~/constants/messages'
+import User from '~/models/schemas/User.schema'
+import { ObjectId } from 'mongodb'
 
-export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body
-  if (email === 'duthanhduoc@gmail.com' && password === '123456') {
-    return res.status(200).json({
-      message: 'Login success'
-    })
-  }
-  return res.status(400).json({
-    error: 'Login failed'
+export const loginController = async (req: Request, res: Response) => {
+  const user = req.user as User
+  const user_id = user._id as ObjectId
+  const result = await usersService.login(user_id.toString())
+  return res.json({
+    message: USERS_MESSAGES.LOGIN_SUCCESS,
+    result
   })
 }
 export const registerController = async (
@@ -21,7 +22,7 @@ export const registerController = async (
 ) => {
   const result = await usersService.register(req.body)
   return res.json({
-    message: 'Register success',
+    message: USERS_MESSAGES.REGISTER_SUCCESS,
     result
   })
 }
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -29,15 +29,22 @@ class UsersService {
       }
     })
   }
+  private signAccessAndRefreshToken(user_id: string) {
+    return Promise.all([this.signAccessToken(user_id), this.signRefreshToken(user_id)])
+  }
   async register(payload: RegisterReqBody) {
     const result = await databaseService.users.insertOne(
       new User({ ...payload, date_of_birth: new Date(payload.date_of_birth), password: hashPassword(payload.password) })
     )
     const user_id = result.insertedId.toString()
-    const [access_token, refresh_token] = await Promise.all([
-      this.signAccessToken(user_id),
-      this.signRefreshToken(user_id)
-    ])
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
+    return {
+      access_token,
+      refresh_token
+    }
+  }
+  async login(user_id: string) {
+    const [access_token, refresh_token] = await this.signAccessAndRefreshToken(user_id)
     return {
       access_token,
       refresh_token
