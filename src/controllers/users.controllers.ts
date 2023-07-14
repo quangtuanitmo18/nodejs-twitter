b114eb0f82bedc4bd4d8f9530688829f635d3cc7
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-07-14 19:18:16 +0300

    tao wraprequestHandler de xu ly loi

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express'
 import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterReqBody } from '~/models/requests/User.requests'
+import { NextFunction, Request, Response } from 'express'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -14,19 +14,14 @@ export const loginController = (req: Request, res: Response) => {
     error: 'Login failed'
   })
 }
-export const registerController = async (req: Request<ParamsDictionary, any, RegisterReqBody>, res: Response) => {
-  const { email, password } = req.body
-  try {
-    const result = await usersService.register(req.body)
-    // await databaseService.users.find({})
-    return res.json({
-      message: 'Register success',
-      result
-    })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'Register failed',
-      error
-    })
-  }
+export const registerController = async (
+  req: Request<ParamsDictionary, any, RegisterReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const result = await usersService.register(req.body)
+  return res.json({
+    message: 'Register success',
+    result
+  })
 }
--- src/user.routes.ts
+++ src/user.routes.ts
@@ -1,26 +1,32 @@
 import { Router } from 'express'
-const userRouter = Router()
+import { loginController, registerController } from '~/controllers/users.controllers'
+import { loginValidator, registerValidator } from '~/middlewares/users.middlewares'
+import { wrapRequestHandler } from '~/utils/handlers'
+const usersRouter = Router()
 
+usersRouter.post('/login', loginValidator, loginController)
 //middleware
-userRouter.use(
-  (req, res, next) => {
-    console.log('Time: ', Date.now())
-    next()
-  },
-  (req, res, next) => {
-    console.log('Time 2: ', Date.now())
-    next()
-  }
-)
-userRouter.get('/tweets', (req, res) => {
-  res.json({
-    data: [
-      {
-        id: 1,
-        text: 'hello world'
-      }
-    ]
-  })
-})
+// userRouter.use(
+//   (req, res, next) => {
+//     console.log('Time: ', Date.now())
+//     next()
+//   },
+//   (req, res, next) => {
+//     console.log('Time 2: ', Date.now())
+//     next()
+//   }
+// )
+// userRouter.get('/tweets', (req, res) => {
+//   res.json({
+//     data: [s
+//       {
+//         id: 1,
+//         text: 'hello world'
+//       }
+//     ]
+//   })
+// })
 
-export default userRouter
+usersRouter.post('/register', registerValidator, wrapRequestHandler(registerController))
+
+export default usersRouter
