f63166263fcb0bafd73aee7d3b02cf359c09f7a8
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-07-18 03:27:26 +0300

    change password

--- src/models/requests/User.requests.ts
+++ src/models/requests/User.requests.ts
@@ -9,7 +9,11 @@ export interface GetProfileReqParams extends ParamsDictionary {
 export interface UnfollowReqParams extends ParamsDictionary {
   followed_user_id: string
 }
-
+export interface ChangePasswordReqBody {
+  old_password: string
+  password: string
+  confirm_password: string
+}
 export interface FollowReqBody {
   followed_user_id: string
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,11 +12,13 @@ import {
   updateMeController,
   getProfileController,
   followController,
-  unfollowController
+  unfollowController,
+  changePasswordController
 } from '~/controllers/users.controllers'
 import { filterMiddleware } from '~/middlewares/common.middlewares'
 import {
   accessTokenValidator,
+  changePasswordValidator,
   emailVerifyTokenValidator,
   followValidator,
   forgotPasswordValidator,
@@ -149,6 +151,20 @@ usersRouter.delete(
   verifiedUserValidator,
   unfollowValidator,
   wrapRequestHandler(unfollowController)
+)
+/**
+ * Description: Change password
+ * Path: /change-password
+ * Method: PUT
+ * Header: { Authorization: Bearer <access_token> }
+ * Body: { old_password: string, password: string, confirm_password: string }
+ */
+usersRouter.put(
+  '/change-password',
+  accessTokenValidator,
+  verifiedUserValidator,
+  changePasswordValidator,
+  wrapRequestHandler(changePasswordController)
 )
 
 export default usersRouter
