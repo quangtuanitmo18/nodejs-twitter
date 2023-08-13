dcd9e3752286b2a6e23470490e40025229745a64
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-08-14 00:01:19 +0300

    serve file with sendfile

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
 import { UPLOAD_DIR } from './constants/dir'
+import staticRouter from './routes/static.routes'
 config()
 
 databaseService.connect()
@@ -16,7 +17,8 @@ initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
-app.use('/static', express.static(UPLOAD_DIR))
+// app.use('/static', express.static(UPLOAD_DIR))
+app.use('/static', staticRouter)
 
 app.use(defaultErrorHandler)
 app.listen(port, () => {
