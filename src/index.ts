996e90f942814642f1dae8c9221710284f3aee6e
quangtuanitmo18 <quangtuanitmo18@gmail.com>
2023-08-13 23:24:08 +0300

    init folder upload

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import usersRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
 import mediasRouter from './routes/medias.routes'
+import { initFolder } from './utils/file'
 databaseService.connect()
 const app = express()
 const port = 4000
 
+initFolder()
 app.use(express.json())
 app.use('/users', usersRouter)
 app.use('/medias', mediasRouter)
